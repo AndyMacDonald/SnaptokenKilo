59f9cdccc9994dcadc4105b3227c260ffebf66b7
Andrew MacDonald <awm@alum.mit.edu>
2019-02-04 19:34:53 -0800

    Cursor movement respects tabs (steps 85-89)

--- src/kilo.c
+++ src/kilo.c
@@ -43,6 +43,7 @@ typedef struct erow {
 
 struct editor_config {
   int cx, cy;
+  int rx;
   int rowoff;
   int coloff;
   int screenrows;
@@ -166,6 +167,17 @@ int get_window_size(int* rows, int *cols) {
 
 /*** row operations ***/
 
+int editor_row_cx_to_rx(erow *row, int cx) {
+  int rx = 0;
+  int j;
+  for (j= 0; j < cx; j++) {
+    if (row->chars[j] == '\t')
+      rx += (KILO_TAB_STOP - 1) - (rx % KILO_TAB_STOP);
+    rx++;
+  }
+  return rx;
+}
+
 void editor_update_row(erow *row) {
   int tabs = 0;
   int j;
@@ -250,17 +262,22 @@ void ab_free(struct abuf *ab) {
 /*** output ***/
 
 void editor_scroll() {
+  E.rx = 0;
+  if (E.cy < E.numrows) {
+    E.rx = editor_row_cx_to_rx(&E.row[E.cy], E.cx);
+  }
+
   if (E.cy < E.rowoff) {
     E.rowoff = E.cy;
   }
   if (E.cy >= E.rowoff + E.screenrows) {
     E.rowoff = E.cy - E.screenrows + 1;
   }
-  if (E.cx < E.coloff) {
-    E.coloff = E.cx;
+  if (E.rx < E.coloff) {
+    E.coloff = E.rx;
   }
-  if (E.cx >= E.coloff + E.screencols) {
-    E.coloff = E.cx - E.screencols + 1;
+  if (E.rx >= E.coloff + E.screencols) {
+    E.coloff = E.rx - E.screencols + 1;
   }
 }
 
@@ -310,7 +327,7 @@ void editor_refresh_screen() {
 
   char buf[32];
   snprintf(buf, sizeof(buf), "\x1b[%d;%dH", (E.cy - E.rowoff) + 1,
-	                                    (E.cx - E.coloff) + 1);
+	                                    (E.rx - E.coloff) + 1);
   ab_append(&ab, buf, strlen(buf));
 
   ab_append(&ab, "\x1b[?25h", 6);
@@ -401,6 +418,7 @@ void editor_process_keypress() {
 void init_editor() {
   E.cx = 0;
   E.cy = 0;
+  E.rx = 0;
   E.rowoff = 0;
   E.coloff = 0;
   E.numrows = 0;
