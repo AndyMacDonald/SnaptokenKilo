af3c6707c8a98710f2b81bf7d1cd98fd8791b2b2
Andrew MacDonald <awm@alum.mit.edu>
2019-02-04 18:13:58 -0800

    Render tabs as spaces (steps 80-84)

--- src/kilo.c
+++ src/kilo.c
@@ -16,6 +16,7 @@
 /*** defines ***/
 
 #define KILO_VERSION "0.0.1"
+#define KILO_TAB_STOP 8
 
 #define CTRL_KEY(k) ((k) & 0x1f)
 
@@ -35,7 +36,9 @@ enum editor_key {
 
 typedef struct erow {
   int size;
+  int rsize;
   char *chars;
+  char *render;
 } erow;
 
 struct editor_config {
@@ -163,6 +166,29 @@ int get_window_size(int* rows, int *cols) {
 
 /*** row operations ***/
 
+void editor_update_row(erow *row) {
+  int tabs = 0;
+  int j;
+  for (j = 0; j < row->size; j++) {
+    if (row->chars[j] == '\t') tabs++;
+  }
+
+  free(row->render);
+  row->render = malloc(row->size + tabs * (KILO_TAB_STOP - 1) + 1);
+
+  int idx = 0;
+  for (j = 0; j < row->size; j++) {
+    if (row->chars[j] == '\t') {
+      row->render[idx++] = ' ';
+      while (idx % KILO_TAB_STOP != 0) row->render[idx++] = ' ';
+    } else {
+      row->render[idx++] = row->chars[j];
+    }
+  }
+  row->render[idx] = '\0';
+  row->rsize = idx;
+}
+
 void editor_append_row(char *s, size_t len) {
   E.row = realloc(E.row, sizeof(erow) * (E.numrows + 1));
 
@@ -171,6 +197,11 @@ void editor_append_row(char *s, size_t len) {
   E.row[at].chars = malloc(len + 1);
   memcpy(E.row[at].chars, s, len);
   E.row[at].chars[len] = '\0';
+
+  E.row[at].rsize = 0;
+  E.row[at].render = NULL;
+  editor_update_row(&E.row[at]);
+
   E.numrows++;
 }
 
@@ -254,10 +285,10 @@ void editor_draw_rows(struct abuf *ab) {
 	ab_append(ab, "~", 1);
       }
     } else {
-      int len = E.row[filerow].size - E.coloff;
+      int len = E.row[filerow].rsize - E.coloff;
       if (len < 0) len = 0;
       if (len > E.screencols) len = E.screencols;
-      ab_append(ab, &E.row[filerow].chars[E.coloff], len);
+      ab_append(ab, &E.row[filerow].render[E.coloff], len);
     }
 
     ab_append(ab, "\x1b[K", 3);
